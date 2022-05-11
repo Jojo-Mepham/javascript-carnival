7f7c8e3f5a4ab5bc13c01730fc47cae4f7dd3e16
Jojo-Mepham <jomepham@gmail.com>
2022-05-12 11:08:04 +1200

    create mole array

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,9 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+let moleTable = document.getElementsByTagName('TD') // get table array
+let moleTableArr = Array.from(moleTable) // turn table into js array so I can access index
+let randomMole = moleTableArr[Math.floor(Math.random() * moleTableArr.length)] // generate random index selector from array
+console.log(randomMole)
