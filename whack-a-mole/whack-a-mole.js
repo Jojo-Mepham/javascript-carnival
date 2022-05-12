d130efd4f52aa609d18b16c768e45b7e5c0a3df7
Jojo-Mepham <jomepham@gmail.com>
2022-05-12 17:02:05 +1200

    refactor

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -4,8 +4,8 @@
 
 console.log('Whack-a-Mole!')
 let moleTable = document.getElementsByTagName('TD') // get the TD elements in a variable
-let moleTableArr = Array.from(moleTable) // turn the DOM array into a JS array
-let randomMoleArr = [Math.floor(Math.random() * moleTableArr.length)] // get a random number which is the index of each cell
+//let moleTableArr = Array.from(moleTable) // turn the DOM array into a JS array //-- REFACTOR removed moleTableArr after I finished when I was looking over my code and realised I didn't need to convert moleTable as it was already an array. Added DOM arrays into my study break/revision week notes to understand a bit better.
+let randomMoleArr = [Math.floor(Math.random() * moleTable.length)] // get a random number which is the index of each cell
 let randomMole = moleTable[randomMoleArr] // assign the random number to a variable
 let moleImage = document.createElement('IMG') // create a new image element
 moleImage.src = 'mole.png' // assig an image source to the image element
@@ -13,7 +13,7 @@ moleImage.classList.add('mole') // assig a class style to the image element so i
 document.getElementsByTagName('TD')[randomMoleArr].appendChild(moleImage) // assign my mole image to a cell in the table
 
 let whackMole = () => {
-  randomMoleArr = [Math.floor(Math.random() * moleTableArr.length)]
+  randomMoleArr = [Math.floor(Math.random() * moleTable.length)]
   randomMole = moleTable[randomMoleArr]
   randomMole.appendChild(moleImage)
   let whackSound = document.createElement('audio')
