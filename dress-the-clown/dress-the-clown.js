c4bb26d8152af67f5b2d43943b5e7d18295cd428
Jojo-Mepham <jomepham@gmail.com>
2022-05-13 15:11:23 +1200

    clownHeadIndex Conditional

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -6,8 +6,9 @@ console.log('Dress The Clown!')
 
 let clownHead = document.getElementById('head')
 let clownHeadFileName = './images/head'
-let clownHeadIndex = 0
+let clownHeadIndex = 3
 let clownHeadFileType = '.png'
+
 let changeClownHead = () => {
   let clownHeadSrc = clownHeadFileName.concat(
     '',
@@ -17,6 +18,24 @@ let changeClownHead = () => {
   )
   return clownHeadSrc
 }
+let increaseClownHeadIndex = () => {
+  if (clownHeadIndex <= 0) {
+    clownHeadIndex = 1
+  } else if (clownHeadIndex <= 1) {
+    clownHeadIndex = 2
+  } else if (clownHeadIndex <= 2) {
+    clownHeadIndex = 3
+  } else if (clownHeadIndex <= 3) {
+    clownHeadIndex = 4
+  } else if (clownHeadIndex <= 4) {
+    clownHeadIndex = 5
+  } else {
+    clownHeadIndex = 0
+  }
+  return clownHeadIndex
+}
 
 console.log(changeClownHead())
 console.log(clownHead)
+console.log(clownHeadIndex)
+console.log(increaseClownHeadIndex())
